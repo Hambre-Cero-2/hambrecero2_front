8ab63eb5f80a46ca214a17583482fd516027c6ab
Paularicarte <ricarte.paula@gmail.com>
2026-02-04 20:57:06 +0100

    get by id receta y post receta

--- src/main/java/com/example/recipeapp/controller/RecipeController.java
+++ src/main/java/com/example/recipeapp/controller/RecipeController.java
@@ -3,7 +3,14 @@ package com.example.recipeapp.controller;
 import com.example.recipeapp.dto.RecipeOutDto;
 import com.example.recipeapp.service.RecipeService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.recipeapp.dto.RecipeInDto;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 import java.util.List;
 
@@ -16,8 +23,19 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
+    @GetMapping("/recipes/{id}")
+    public RecipeOutDto getRecipeById(@PathVariable Long id) {
+        return recipeService.findById(id);
+    }
+
     @GetMapping("/recipes")
     public List<RecipeOutDto> getAllRecipes() {
         return recipeService.findAll();
     }
+    
+    @PostMapping("/recipes")
+    public RecipeOutDto createRecipe(@Valid @RequestBody RecipeInDto inDto) {
+         return recipeService.create(inDto);
+    }
+   
 }
--- src/main/java/com/example/recipeapp/service/RecipeService.java
+++ src/main/java/com/example/recipeapp/service/RecipeService.java
@@ -1,9 +1,14 @@
 package com.example.recipeapp.service;
 
+import com.example.recipeapp.domain.Ingredient;
+import com.example.recipeapp.domain.Recipe;
+import com.example.recipeapp.dto.RecipeInDto;
 import com.example.recipeapp.dto.RecipeOutDto;
+import com.example.recipeapp.exception.RecipeNotFoundException;
+import com.example.recipeapp.repository.IngredientRepository;
 import com.example.recipeapp.repository.RecipeRepository;
-import org.springframework.stereotype.Service;
 import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
@@ -11,10 +16,12 @@ import java.util.List;
 public class RecipeService {
 
     private final RecipeRepository recipeRepository;
+    private final IngredientRepository ingredientRepository;
     private final ModelMapper modelMapper;
 
-    public RecipeService(RecipeRepository recipeRepository, ModelMapper modelMapper) {
+    public RecipeService(RecipeRepository recipeRepository, IngredientRepository ingredientRepository, ModelMapper modelMapper) {
         this.recipeRepository = recipeRepository;
+        this.ingredientRepository = ingredientRepository;
         this.modelMapper = modelMapper;
     }
 
@@ -23,5 +30,30 @@ public class RecipeService {
                 .stream()
                 .map(r -> modelMapper.map(r, RecipeOutDto.class))
                 .toList();
+    }
+
+    public RecipeOutDto create(RecipeInDto inDto) {
+        Recipe recipe = new Recipe();
+        recipe.setName(inDto.name);
+        recipe.setDifficulty(inDto.difficulty);
+        recipe.setVegetarian(inDto.vegetarian);
+        recipe.setEstimatedCost(inDto.estimatedCost);
+        recipe.setLastModified(inDto.lastModified);
+        recipe.setServings(inDto.servings);
+
+        List<Ingredient> ingredients = ingredientRepository.findAllById(inDto.ingredientIds);
+
+        if (ingredients.size() != inDto.ingredientIds.size()) {
+            throw new IllegalArgumentException("Some ingredientIds do not exist");
+        }
+
+        recipe.setIngredients(ingredients);
+        Recipe saved = recipeRepository.save(recipe);
+        return modelMapper.map(saved, RecipeOutDto.class);
+    }
+    public RecipeOutDto findById(Long id) {
+            Recipe recipe = recipeRepository.findById(id)
+            .orElseThrow(() -> new RecipeNotFoundException(id));
+            return modelMapper.map(recipe, RecipeOutDto.class);
     }
 }
