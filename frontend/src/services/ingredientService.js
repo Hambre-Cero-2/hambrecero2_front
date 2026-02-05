55ee4b95e0aa0678002344ae7feb191fec57df70
Paularicarte <ricarte.paula@gmail.com>
2026-02-05 15:47:19 +0100

    completar los servicios de ingredinetes

--- frontend/src/services/ingredientService.js
+++ frontend/src/services/ingredientService.js
@@ -1,13 +1,25 @@
 import api from './api';
 
 export const getIngredients = async () => {
-  const response = await api.get("/ingredients");
-  return response.data;
+  const { data } = await api.get('/ingredients');
+  return data;
 };
+
 export const getIngredientById = async (id) => {
-  const response = await api.get(`/ingredients/${id}`);
-  return response.data;
+  const { data } = await api.get(`/ingredients/${id}`);
+  return data;
 };
 
+export const createIngredient = async (payload) => {
+  const { data } = await api.post('/ingredients', payload);
+  return data;
+};
 
+export const updateIngredient = async (id, payload) => {
+  const { data } = await api.put(`/ingredients/${id}`, payload);
+  return data;
+};
 
+export const deleteIngredient = async (id) => {
+  await api.delete(`/ingredients/${id}`);
+};
